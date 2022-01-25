80b4f38e7201136606713e7bbca7eba5e32880d5
KonstantinaStrantzali <konstrantz@gmail.com>
2022-01-25 12:52:35 +0000

    desctructured the array results to make the code cleaner and access the data of the objects easier. For each quiz answer set up an Event Listener to control user's interaction with the quiz when pressing answers. Each button pressed, changing the background respectivly depending the correct and incorrect result. The score increases each time the user answers correctly and displayed.

--- assets/quiz.js
+++ assets/quiz.js
@@ -4,11 +4,11 @@ let answer2 = document.getElementById("option2");
 let answer3 = document.getElementById("option3");
 let answer4 = document.getElementById("option4");
 let quizAnswers = document.getElementsByClassName("answers");
+let scoreCounter = document.getElementById("scoreCounter");
 let data;
-
-
-
-window.onload = sendApiRequest;
+let round = 0;
+let score = 0;
+question.innerText = round +=1;
 
 async function sendApiRequest(){
     let response = await fetch("https://opentdb.com/api.php?amount=10");
@@ -16,54 +16,41 @@ async function sendApiRequest(){
     console.log(data);
     displayApiData(data);
 }
-
-const timeOut = function() {
-    console.log("I am waiting");
-}
+window.onload = sendApiRequest; 
 
 function displayApiData(data){
-    question.innerHTML = `${data.results[0].question}`;
-    answer1.innerHTML = `${data.results[0].incorrect_answers[0]}`;
-    answer2.innerHTML = `${data.results[0].incorrect_answers[1]}`;
-    console.log(answer2);
-    answer3.innerHTML = `${data.results[0].incorrect_answers[2]}`;
-    answer4.innerHTML = `${data.results[0].correct_answer}`;
-    }
-//check if its correct or incorect property name
-
-   /* quizAnswers.addEventListener("click", ()=>{
-        for(i =0; i<quizAnswers.length; quizAnswers++){
-            if(this.quizAnswers[i] === "correct_answer"){
-                  quizAnswer[i].style.color = "green";
-            }
-        }
-    })  */
+    const results = data.results[round]; //data destructured 
     
-for (let i = 0; i < quizAnswers.length; i++) {
-    quizAnswers[i].addEventListener("click", function () {
-        if(quizAnswers[i].textContent === data.results[0].correct_answer){
-            quizAnswers[i].style.backgroundColor ="green";
-        }
-        else {
-            quizAnswers[i].style.backgroundColor = "red";
-        }  
-    });
+    question.innerHTML = `${results.question}`;
+    answer1.innerHTML = `${results.incorrect_answers}`;
+    answer2.innerHTML = `${results.incorrect_answers[1]}`;
+    console.log(answer2);
+    answer3.innerHTML = `${results.incorrect_answers[2]}`;
+    answer4.innerHTML = `${results.correct_answer}`;
 }
 
-function newQuestion (questionData){
-    for (let answer of quizAnswers){
-        answer.style.backgroundColor= "grey"
-    }
-    let questionMap = new Map(quizAnswers.entries())
-    let possibleAnswers = questionData.incorrect_answers
-    possibleAnswers.push(questionData.correct_answer)
-    const shuffledAnswers = possibleAnswers.sort(() => Math.random() - 0.5);
-    console.log(shuffledAnswers);
+[...quizAnswers].forEach((qa) => qa.addEventListener("click", function(event) {
+    
+let isCorrect = event.target.innerText === data.results[round].correct_answer;
+if (isCorrect) score+= 100;
+scoreCounter.innerText = score;
 
-    if (questionData.type === 'multiple'){
-        question.textContent = questionData.question;
-        for(i=0; i < shuffledAnswers.length; i++){
-            quizAnswers[i].textContent = shuffledAnswers[i]
-        }
+event.target.style.backgroundColor = isCorrect ? "green" : "red";
+
+
+setTimeout (() =>{
+    event.target.style.backgroundColor = "grey"
+    round++;
+    displayApiData(data)
+}, 2000);
+
+}));
+
+
+
+function increaseScore(){
+    if(data.results[round].correct_answer){
+        score.innerText += 100;
     }
+
 }
