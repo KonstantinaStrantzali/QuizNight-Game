bdcf20a1917ae22c8858e24fdc8eba966bed0618
KonstantinaStrantzali <konstrantz@gmail.com>
2022-02-18 17:33:18 +0000

    final commit code pretified

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,3 +1,17 @@
+//Retrieve data and display the score
+const recentScore = localStorage.getItem(`recentScore`);
+const finalScore = document.querySelector("#quiz-finalscore");
+finalScore.innerText = recentScore;
+// retrieve answers
+const userAnswers = localStorage.getItem("userAnswers");
+const userChoices = JSON.parse(userAnswers);
+//retrieve questions-correct_answers and display them
+const data = localStorage.getItem(`getAnswers`);
+let getAnswers = [];
+if (data) {
+	getAnswers = JSON.parse(data);
+}
+
 //Build a table and display the data
 let myTable = document.querySelector('#table');
 
@@ -14,16 +28,16 @@ headers.forEach(headerText => {
 table.appendChild(headerRow);
 getAnswers.forEach((obj, i) => {
 	let objRow = document.createElement('tr');
-
+	
 	let cell1 = document.createElement('td');
 	let cell2 = document.createElement('td');
 	let cell3 = document.createElement('td');
-
+	
 	cell1.innerHTML = obj.question;
-	cell2.innerHTML = obj.correct;
+	cell2.innerHTML= obj.correct;
 	cell3.innerHTML = userChoices[i];
-	//Find the correct answers and turn them green
-	if (cell2.innerHTML !== cell3.innerHTML) {
+//Find the correct answers and turn them green
+	if (cell2.innerHTML !== cell3.innerHTML ){
 		cell3.style.background = "#CE3854ff";
 	} else {
 		cell3.style.background = "green";
@@ -33,7 +47,7 @@ getAnswers.forEach((obj, i) => {
 	objRow.appendChild(cell3);
 
 	table.appendChild(objRow);
-
+	
 });
 
 myTable.appendChild(table);
